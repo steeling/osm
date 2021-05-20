3910dc8971d58dfac998c5d48ed23d8fbc5a29b2
cmgit <chmar@microsoft.com>
2021-05-20 19:34:06 +0000

    First smoke test of e2e meshconfig

---
+++ tests/e2e/e2e_mesh_config_validator_test.go
@@ -0,0 +1,35 @@
+package e2e
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	config "github.com/openservicemesh/osm/pkg/apis/config/v1alpha1"
+	. "github.com/openservicemesh/osm/tests/framework"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	testMeshName = "meshconfigtest"
+)
+
+var _ = OSMDescribe("Test Submit Mesh Config Policy",
+	OSMDescribeInfo{
+		Tier:   2,
+		Bucket: 5,
+	},
+
+	func() {
+		Context("MeshConfigValidator", func() {
+			It("Tests a valid Mesh Config Policy", func() {
+				ctx := context.TODO()
+				meshIn := new(config.MeshConfig)
+				meshClient := Td.ConfigClient
+				meshIn.Name = testMeshName
+				meshRet, err := meshClient.ConfigV1alpha1().MeshConfigs(Td.OsmNamespace).Create(ctx, meshIn, v1.CreateOptions{})
+				Expect(meshRet).ShouldNot(BeNil())
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+		})
+	})
