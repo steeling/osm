05b2466417eb961ed691151757d05c07c0d8804f
Sean Teeling <seanteeling@microsoft.com>
2022-09-10 13:29:36 -0400

    start
    Signed-off-by: Sean Teeling <seanteeling@microsoft.com>

---
+++ pkg/envoy/generator/sds/types.go
@@ -0,0 +1,10 @@
+// Package sds implements Envoy's Secret Discovery Service (SDS).
+package sds
+
+import (
+	"github.com/openservicemesh/osm/pkg/logger"
+)
+
+var (
+	log = logger.New("envoy/sds")
+)
